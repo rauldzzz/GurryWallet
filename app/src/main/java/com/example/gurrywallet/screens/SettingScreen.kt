ceb2c2d760b4ec8b22234a5c188ea46e328ac334
rauldzzz <raul.diaz01@estudiant.upf.edu>
2026-03-24 20:22:25 +0100

    Settings uxui

--- app/src/main/java/com/example/gurrywallet/screens/SettingScreen.kt
+++ app/src/main/java/com/example/gurrywallet/screens/SettingScreen.kt
@@ -1,21 +1,37 @@
 package com.example.gurrywallet.screens
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gurrywallet.SettingsViewModel
 import com.example.gurrywallet.components.ScreenTitle
 import com.example.gurrywallet.database.UserEntity
@@ -39,22 +55,93 @@ fun SettingsContent(
     onAction: (SettingsViewModel.SettingsAction) -> Unit,
     currentUser: StateFlow<UserEntity?>
 ) {
+    val user by currentUser.collectAsState()
     GurryWalletTheme {
         Scaffold(
             topBar = {
                 ScreenTitle("Settings")
             },
             modifier = Modifier.fillMaxSize()
-        ){ paddingValues ->
+        ) { paddingValues ->
             Column(
                 modifier = Modifier
                     .padding(
                         top = paddingValues.calculateTopPadding()
                     )
+                    .padding(horizontal = 24.dp)
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp), // Altura de la carta
+                    shape = RoundedCornerShape(16.dp), // Bordes muy redondeados
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp), // Padding interno para las flechas
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween // Separa los elementos a los extremos
+                    ) {
+                        // Flecha Izquierda
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                            contentDescription = "Last User",
+                            modifier = Modifier
+                                .size(32.dp)
+                                .clickable {
+                                    onAction(SettingsViewModel.SettingsAction.onPreviousUserClick)
+                                },
+                            tint = MaterialTheme.colorScheme.tertiary
+                        )
 
+                        // Nombre del Usuario Actual
+                        Text(
+                            text = user?.id ?: "Loading...",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.tertiary
+                        )
+
+                        // Flecha Derecha
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                            contentDescription = "Next User",
+                            modifier = Modifier
+                                .size(32.dp)
+                                .clickable {
+                                    onAction(SettingsViewModel.SettingsAction.onNextUserClick)
+                                },
+                            tint = MaterialTheme.colorScheme.tertiary
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)// Altura de la carta
+                        .clickable {
+                            /*TODO Add tutorial*/
+                        },
+                    shape = RoundedCornerShape(16.dp), // Bordes muy redondeados
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)
+                ){
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "How to use",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.tertiary
+                        )
+                    }
+                }
             }
         }
     }
